65f4e8ffe68eb7331300db612a5bf170e0a3ea90
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-08-03 15:30:23 +0530

    Update id_maker.py

--- VOID/plugins/id_maker.py
+++ VOID/plugins/id_maker.py
@@ -14,9 +14,9 @@ async def image_maker(event) -> None:
     # open id photo
     id_template = Image.open("GIT.png")
     # resize user photo to fit box in id template
-    user_photo = user_photo.resize((500, 500))
+    user_photo = user_photo.resize((1159, 1241))
     # put image in position
-    id_template.paste(user_photo, (500, 200))
+    id_template.paste(user_photo, (1003, 641))
     # postion on where to draw text
     draw = ImageDraw.Draw(id_template)
     color = "rgb(0, 0, 0)"  # black
