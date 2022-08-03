af103f0d7d4f3320b5e490a97cbaa7d0115fb65a
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-08-03 16:33:57 +0530

    Update id_maker.py

--- VOID/plugins/id_maker.py
+++ VOID/plugins/id_maker.py
@@ -29,7 +29,7 @@ async def image_maker(event) -> None:
         fill=color,
         font=font2,
     )
-    draw.text((380, 40), str(replied_user.from_id.user_id), fill=color, font=font)
+    draw.text((3, 35), str(replied_user.from_id.user_id), fill=color, font=font)
     id_template.save("user_id.png")
     if "doc" in event.text:
         force_document = True
