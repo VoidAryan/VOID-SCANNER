9efaff883ada3c49b47a53655dd3b9356db536bc
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-08-04 14:34:16 +0530

    Create test.py

---
+++ VOID/plugins/test.py
@@ -0,0 +1,25 @@
+help_plus = """
+🔰 ʙᴀɴᴄᴏᴅᴇꜱ ᴏғ ᴠᴏɪᴅ ꜱʏꜱᴛᴇᴍ :-
+۞ `{Vx01}` - SCAMMER
+۞ `{Vx02}` - COMMUNITY HATE.
+۞ `{Vx03}` - ABUSE SPAM.
+۞ `{Vx04}` - NSFW SPAMMER.
+۞ `{Vx05}` - IMPERSONATION.
+۞ `{Vx06}` - MD/BTC SCAM.
+۞ `{Vx07}` - ADDING SPAMBOTS.
+۞ `{Vx08}` - ILLEGAL.
+۞ `{Vx09}` - PHISHING.
+۞ `{Vx10}` - FRAUD PROMOTIO.
+۞ `{Vx11}` - CYBER THREATENING / BULLY.
+۞ `{Vx12}` - CHILD ABUSE.
+۞ `{Vx13}` - BAN EVASION.
+۞ `{Vx14}` - SPAMBOT.
+۞ `{Vx15}` - RAID INITIALIZOR. 
+۞ `{Vx16}` - RAID PARTICIPANT.
+۞ `{Vx17}` - KRIMINALANT.
+۞ `{Vx18}` - RAID/SPAM INFLAMMER.
+۞ `{Vx20}` - HATE AGAINST COMMUNITY
+BY : @VoidxNetwork
+"""
+
+__plugin_name__ = "Test"
