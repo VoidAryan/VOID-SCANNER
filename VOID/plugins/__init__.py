c6e2b764cb3b19dc5a26471b173eb6b485d01ceb
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-08-04 16:50:39 +0530

    Update __init__.py

--- VOID/plugins/__init__.py
+++ VOID/plugins/__init__.py
@@ -1,12 +1,11 @@
 # List of plugins to import
 to_load = [
     "main_manager",
-    "whois",
     "extras",
     "safe",
     "eval",
     "id_maker",
-    "test",
+    "void',
     "shell",
     "bot",
     "bancodes",
