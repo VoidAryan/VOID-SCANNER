1fc04290fbfd99655f3166de638473763cc0ba17
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-08-05 11:56:16 +0530

    Update Test.py

--- VOID/plugins/Test.py
+++ VOID/plugins/Test.py
@@ -2,23 +2,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 from VOID import System, system_cmd
 
 
-@System.on(system_cmd(pattern=r"whois"))
-async def whois(event):
-    try:
-        to_get = event.pattern_match.group(1)
-    except Exception:
-        if event.reply:
-            replied = await event.get_reply_message()
-            to_get = int(replied.sender.id)
-        else:
-            return
 
-    try:
-        data = await System(GetFullUserRequest(to_get))
-    await System.send_message(
-        event.chat_id,
-        f"Perma Link: [{data.user.first_name}](tg://user?id={data.user.id})\nUser ID: `{data.user.id}`\nAbout: {data.about}",
-    )
 
 
 help_plus = """ [۞](https://telegra.ph/file/5403e3fb7685bcf8bf7b2.jpg) Here is Help for **Whois** -\n
@@ -27,4 +11,4 @@ help_plus = """ [۞](https://telegra.ph/file/5403e3fb7685bcf8bf7b2.jpg) Here is 
 `/` `?` `.` `!` are supported prefixes.\n
 **Example:** `/addenf` or `?addenf` or `.addenf`
 """
-__plugin_name__ = "Test"
+__plugin_name__ = "whois"
