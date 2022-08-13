daff848870d1c437602e0faba81baea311173885
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-08-13 23:41:48 +0530

    Update __main__.py

--- VOID/__main__.py
+++ VOID/__main__.py
@@ -51,7 +51,7 @@ async def status(event):
     sender = await event.get_sender()
     user_status = "ɪɴꜱᴘᴇᴄᴛᴏʀ" if sender.id in INSPECTORS else "ᴇɴғᴏʀᴄᴇʀ" if sender.id in ENFORCERS else 'ɴᴏʀᴍᴀʟ ᴜꜱᴇʀ'
     if user_status == 'ɴᴏʀᴍᴀʟ ᴜꜱᴇʀ':
-        await event.reply(on_string.format(Enforcer=user_status, name=sender.first_name))
+        await System.send_file(event.chat_id, INFOPIC, caption=on_string.format(Enforcer=user_status, name=sender.first_name))
     else:
         await System.send_file(event.chat_id, INFOPIC, caption=on_string.format(Enforcer=user_status, name=sender.first_name))
     await msg.delete()
