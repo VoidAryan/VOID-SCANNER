b1321c4b0a4500635d4d25248ebf817aae9ff549
VOID <97348572+Voidxtoxic@users.noreply.github.com>
2022-08-14 11:11:40 +0530

    Update __main__.py

--- VOID/__main__.py
+++ VOID/__main__.py
@@ -102,7 +102,7 @@ async def send_help(event):
         await event.reply(msg)
         return
     if help_for in HELP:
-        await System.send_message(HELP[help_for].help_plus)
+        await System.send_file(VOID_IMG, HELP[help_for].help_plus)
     else:
         return
 
